946212e7e6e40aa86dc6f0ddd78fd226713b7bd4
Alex Chiu <alexander.chiu@houseful.co.uk>
2024-11-10 20:06:08 +0000

    fix: tailwind not working on GameRoom root

--- src/libs/game-room/page/GameRoom.tsx
+++ src/libs/game-room/page/GameRoom.tsx
@@ -7,6 +7,8 @@ import {
   type InitialGameRoomContextProps,
 } from "./GameRoom.context";
 
+import "../../../base.css";
+
 interface GameRoomProps extends InitialGameRoomContextProps {}
 
 export const GameRoom = ({
