55f7bf55887cd3db2db38baa3cc81b918c416fe5
Alex Chiu <alexander.chiu@houseful.co.uk>
2025-02-09 10:45:30 +0000

    testing sw notifications..

--- src/libs/game-room/page/GameRoom.tsx
+++ src/libs/game-room/page/GameRoom.tsx
@@ -171,7 +171,6 @@ const Game = ({ creatorId }: { roomName: string; creatorId: string }) => {
                 Reset game
               </button>
             )}
-            <NotificationToggle />
           </div>
         </nav>
 
@@ -574,56 +573,5 @@ const Game = ({ creatorId }: { roomName: string; creatorId: string }) => {
       `
       }</style>
     </>
-  );
-};
-
-const NotificationToggle = () => {
-  const [isNotificationEnabled, setIsNotificationEnabled] = useState<
-    boolean | null
-  >(null);
-
-  useEffect(() => {
-    if (!("Notification" in window)) {
-      // alert("Notifications not supported on this device ");
-      return;
-    }
-
-    if (!isNotificationEnabled) {
-      setIsNotificationEnabled(
-        Boolean(window.Notification.permission === "granted")
-      );
-    }
-  }, [isNotificationEnabled]);
-
-  const toggleNotification = () => {
-    if (!("Notification" in window)) {
-      alert("Notifications not supported on this device. Sorry!");
-      return;
-    }
-
-    if (!(window.Notification.permission === "granted")) {
-      window.Notification.requestPermission().then((permission) => {
-        if (permission === "denied" || permission === "default") {
-          setIsNotificationEnabled(false);
-          alert(
-            "Notifications are disabled on this device. Update permissions in your browser settings to fix this."
-          );
-        }
-
-        setIsNotificationEnabled(true);
-        new Notification("Notifications are enabled!");
-      });
-    }
-    new Notification("Notifications are enabled!");
-  };
-
-  return (
-    <button
-      className="btn btn-ghost"
-      type="button"
-      onClick={toggleNotification}
-    >
-      {isNotificationEnabled ? "🔔" : "🔕"}
-    </button>
   );
 };
